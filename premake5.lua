7e7ae39430971c36ada0188230d2b370f24e8909
salvorizza <40764521+salvorizza@users.noreply.github.com>
2022-08-24 18:58:17 +0200

    Premake

---
+++ premake5.lua
@@ -0,0 +1,24 @@
+project "STB"
+    kind "StaticLib"
+    language "C"
+    staticruntime "off"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "**.h",
+        "**.c"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
